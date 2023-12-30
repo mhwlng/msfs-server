3d6bf4c3ff8b1f82814df0c503d475c414923e90
Marcel Houweling <mhwlng@gmail.com>
2023-12-30 13:52:45 +0100

    css

--- msfs-server/Components/MovingMap.razor.js
+++ msfs-server/Components/MovingMap.razor.js
@@ -89,7 +89,7 @@ export function Init() {
 
     map = L.map('map', {
         center: [51.505, -0.09],
-        zoom: 13,
+        zoom: 12,
         layers: [cartoDBlayer],
         preferCanvas: true
     });
